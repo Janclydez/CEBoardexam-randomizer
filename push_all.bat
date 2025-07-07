2fc6c5758c147ea6b62d9a2422961899429f84a5
Janclydez <lemjanjay@gmail.com>
2025-07-07 12:03:17 +0800

    Frontend update

--- push_all.bat
+++ push_all.bat
@@ -4,7 +4,7 @@ cd /d "C:\Users\Clide\Downloads\websitehomepage"
 git remote set-url origin https://github.com/Janclydez/CEBoardexam-randomizer.git
 git add .
 git commit -m "Frontend update"
-git push origin main
+git push origin main --force
 
 echo ---------------------------------------
 echo Pushing BACKEND...
