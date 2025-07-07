3521881fa4237fc6fdbc4dc879a6460923039d75
Janclydez <lemjanjay@gmail.com>
2025-07-07 12:02:38 +0800

    Frontend update

--- push_all.bat
+++ push_all.bat
@@ -8,6 +8,7 @@ git push origin main
 
 echo ---------------------------------------
 echo Pushing BACKEND...
+cd /d "C:\Users\Clide\Downloads\backend-folder-name"
 git remote set-url origin https://github.com/Janclydez/ce-exam-generator.git
 git add .
 git commit -m "Backend update"
